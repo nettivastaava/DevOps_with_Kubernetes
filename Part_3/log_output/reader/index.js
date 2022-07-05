101bb52fe7531ac63c0bf5361f8bc8cde187add0
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-05 14:31:14 +0300

    3.1

---
+++ Part_3/log_output/reader/index.js
@@ -0,0 +1,50 @@
+const PORT = process.env.PORT || 3000
+
+const axios = require('axios')
+const serviceURL = 'http://kube-ping-pong-svc:2346/pingpong'
+
+const express = require('express')
+
+const fs = require('fs')
+const path = require('path')
+
+const http = require('http');
+
+const app = express()
+const server = http.createServer(app)
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'timestamps.txt')
+
+const helloMessage = process.env.MESSAGE
+
+const random = (Math.random() + 1).toString(36).substring(7)
+
+app.get('/', async (_req, res) => {
+  console.log('requesting...')
+  const data = fs.readFileSync(filePath, 'utf8')
+  try {
+    const pongs = await axios.get(serviceURL)
+    console.log('raw data ', pongs)
+    console.log('got something ', pongs?.data)
+    res.status(200).send(
+      `
+      <div>
+        <div>
+          ${helloMessage}
+        </div>
+        <div>
+          ${data} ${random}
+        </div>
+        <div>
+          pongs ${pongs?.data}
+        </div>
+      </div>`
+    )
+  } catch (error) {
+    res.status(404).send(error)
+  }
+})
+
+server.listen(PORT)
+console.log(`Server running on port ${PORT}`)
---
+++ Part_3/ping-pong/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const sequelize = new Sequelize("pingpong-db", "postgres",
+    process.env.POSTGRES_PASSWORD, {
+    host: "postgres-svc",
+    dialect: "postgres"
+})
+
+class Pong extends Model {}
+  Pong.init({  
+    id: {    
+      type: DataTypes.INTEGER,    
+      primaryKey: true,    
+      autoIncrement: true  
+    },  
+    count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {  
+    sequelize,   
+    timestamps: false,  
+    modelName: 'pong'
+  })
+
+Pong.sync()
+
+app.get('/pingpong', async (_req, res) => {
+  const pong = await Pong.findByPk(1)
+  if (!pong) {
+    const newPong = await Pong.create({count: 0})
+    console.log('NEW PONG ', newPong)
+    res.json(newPong.count)
+  } else {
+    try {
+      console.log('EXISTING PONG ', pong)
+      pong.count = pong.count + 1
+
+      await pong.save()
+      res.json(pong.count)
+    } catch(exception) {
+      res.send(exception)
+    }
+  }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
