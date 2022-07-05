21226566cbd8f61e16ba4315f4de3f3103e5188a
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-05 15:56:58 +0300

    3.2

--- Part_3/ping-pong/index.js
+++ Part_3/ping-pong/index.js
@@ -48,7 +48,7 @@ app.get('/pingpong', async (_req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
