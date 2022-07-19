a0c51c37241b54e7892a0f46cdae2f87c3b956f4
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-19 13:29:55 +0300

    4.2

---
+++ Part_4/todo-project/todo-app/util/db.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize("todo-db", "postgres",
+    process.env.POSTGRES_PASSWORD, {
+    host: "postgres-svc",
+    dialect: "postgres"
+})
+
+const connectToDatabase = async () => {
+    try {
+      await sequelize.authenticate()
+      console.log('database connected')
+      return true
+    } catch (error) {
+      console.log('connecting database failed')
+      return false
+    }
+  }
+  
+  module.exports = { sequelize, connectToDatabase }
